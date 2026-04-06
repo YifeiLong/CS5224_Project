86e48b4052d381118e4644f4d7e1ebe7dd1c39fa
Eva <100598075+YifeiLong@users.noreply.github.com>
2026-04-07 02:32:07 +0800

    Add files via upload

---
+++ core_model/evaluation.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import json
+from prophet.serialize import model_from_json
+
+
+def load_and_predict_sunshine(months=12):
+    with open('./model/model_rain.json', 'r') as fin:
+        model_rain = model_from_json(json.load(fin))
+    with open('./model/model_sun.json', 'r') as fin:
+        model_sun = model_from_json(json.load(fin))
+
+    future_dates = model_rain.make_future_dataframe(periods=months, freq='MS')
+    forecast_rain = model_rain.predict(future_dates)
+
+    future_sun = forecast_rain[['ds', 'yhat']].rename(columns={'yhat': 'no_of_rainy_days'})
+    forecast_sun = model_sun.predict(future_sun)
+
+    df_sun = forecast_sun[['ds', 'yhat']].tail(months).rename(columns={'yhat': 'H_Sunshine_Hrs'})
+    return df_sun.reset_index(drop=True)
+
+
+def load_and_predict_tariff(user_type='domestic', months=12):
+    filename = f'./model/tariff_{user_type}.json'
+    with open(filename, 'r') as fin:
+        model_tariff = model_from_json(json.load(fin))
+
+    future_dates = model_tariff.make_future_dataframe(periods=months, freq='MS')
+    forecast_tariff = model_tariff.predict(future_dates)
+
+    df_tariff = forecast_tariff[['ds', 'yhat']].tail(months).rename(columns={'yhat': 'Tariff_Cents_per_kWh'})
+    return df_tariff.reset_index(drop=True)
+
+
+def calculate_solar_roi(roof_area_m2, user_type='domestic', months_to_predict=12):
+    df_sun = load_and_predict_sunshine(months_to_predict)
+    df_tariff = load_and_predict_tariff(user_type, months_to_predict)
+    df_results = pd.merge(df_sun, df_tariff, on='ds')
+
+    A_eff = roof_area_m2 * 0.7
+    G = 1.0
+    eta_panel = 0.8
+    eta_system = 0.8
+
+    df_results['D_Days'] = df_results['ds'].dt.daysinmonth
+    df_results['E_month_kWh'] = (
+            df_results['H_Sunshine_Hrs'] * A_eff * G * eta_panel * eta_system * df_results['D_Days']
+    )
+
+    df_results['Tariff_Dollars'] = df_results['Tariff_Cents_per_kWh'] / 100.0
+    df_results['Savings_Dollars'] = df_results['E_month_kWh'] * df_results['Tariff_Dollars']
+    df_results['Cumulative_Savings'] = df_results['Savings_Dollars'].cumsum()
+
+    df_results['Date'] = df_results['ds'].dt.strftime('%Y-%m')
+    final_cols = ['Date', 'H_Sunshine_Hrs', 'D_Days', 'E_month_kWh', 'Tariff_Cents_per_kWh', 'Savings_Dollars',
+                  'Cumulative_Savings']
+
+    return df_results[final_cols]
+
+
+
+if __name__ == "__main__":
+    ROOF_AREA = 100
+    USER_TYPE = 'domestic'
+
+    roi_df = calculate_solar_roi(roof_area_m2=ROOF_AREA, user_type=USER_TYPE, months_to_predict=12)
+    pd.options.display.float_format = '{:.2f}'.format
+    print(roi_df)
+
+    total_yield = roi_df['E_month_kWh'].sum()
+    total_savings = roi_df['Savings_Dollars'].sum()
+    print(f"\nYearly Yield: {total_yield:.2f} kWh")
+    print(f"Yearly Savings: ${total_savings:.2f}")
